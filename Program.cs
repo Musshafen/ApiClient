df8ce99f4a6a37cf64111981791f6f6eae473ef4
KM Musshafen <kmusshafen@hotmail.com>
2023-04-30 16:42:41 -0400

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+
+        }
     }
-  }
 }
