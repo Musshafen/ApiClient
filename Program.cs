4922672f21be884f5ffe6d91fcc5d94c53dfd395
KM Musshafen <kmusshafen@hotmail.com>
2023-05-02 20:01:24 -0400

    Commit

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseBodyAsString = client.GetStringAsync("https://api.openbrewerydb.org/v1/breweries");
+            var responseBodyAsString = await client.GetStringAsync("https://api.openbrewerydb.org/v1/breweries");
 
 
         }
